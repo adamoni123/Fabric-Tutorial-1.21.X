7fb16b848368c9c3a40e09254856169a75737055
adam <adamfogel08@gmail.com>
2025-01-17 17:51:12 +0200

    mod items2 electric boogaloo (currently doesn't work)

--- src/main/java/net/adamoni/tutorialmod/item/ModItems.java
+++ src/main/java/net/adamoni/tutorialmod/item/ModItems.java
@@ -6,13 +6,20 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
 
 
     private static Item registerItem(String name, Item item) {
+        Item.Settings itemSettings = new Item.Settings();
+        Identifier id = Identifier.of(TutorialMod.MOD_ID, "pink_garnet");
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
+        Item.Settings settings = itemSettings.registryKey(key);
+        PINK_GARNET = new Item(settings);
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
     }
 
